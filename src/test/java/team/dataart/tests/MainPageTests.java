32236627f7eff2387c27d7af1259b88dbffa94bc
Gorbatenko Vitaliy <gorvit.dp@gmail.com>
2021-05-04 21:05:19 +0300

    add ui tests

--- src/test/java/team/dataart/tests/MainPageTests.java
+++ src/test/java/team/dataart/tests/MainPageTests.java
@@ -1,6 +1,5 @@
 package team.dataart.tests;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.ElementsCollection;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Owner;
@@ -9,7 +8,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 import static team.dataart.tests.TestData.getWebUrl;
@@ -46,44 +45,63 @@ public class MainPageTests extends TestBase {
     @Test
     @DisplayName("Filter vacancies by industry is work")
     void checkIndustryFilter() {
+        String industry = randomValueFromVariant("Интернет вещей", "Медиа и развлечения", "Медицина",
+                "Путешествия", "Ритейл", "Финансы");
+
         step("Select industry", () -> {
-            // todo
+            $("#customSelect__industry").click();
+            $$(".isOpen .customSelect__option").findBy(text(industry)).click();
         });
 
         step("Check vacancy industry", () -> {
-            // todo
+            ElementsCollection allVacancies = $$(".vacanciesWidget__list-item");
+            allVacancies.filter(text(industry)).shouldHaveSize(allVacancies.size());
         });
     }
 
     @Test
     @DisplayName("Filter vacancies by location is work")
     void checkLocationFilter() {
+        String location = randomValueFromVariant("София", "Вроцлав", "Remote.RU", "Remote.UA", "Днепр", "Херсон");
+
         step("Select location", () -> {
-            // todo
+            $("#customSelect__location").click();
+            $$(".isOpen .customSelect__option").findBy(text(location)).click();
         });
 
         step("Check vacancy location", () -> {
-            // todo
+            ElementsCollection allVacancies = $$(".vacanciesWidget__list-item");
+            allVacancies.filter(text(location)).shouldHaveSize(allVacancies.size());
         });
     }
 
     @Test
     @DisplayName("Empty vacancies list has default text")
     void checkThatEmptyVacanciesListHasDefaultText() {
+        String technology = "QA";
+        String industry = "Ставки и игорный бизнес";
+        String location = "София";
+
         step("Select technology", () -> {
-            // todo QA
+            step("Select technology", () -> {
+                $("#customSelect__technology").click();
+                $$(".isOpen .customSelect__option").findBy(text(technology)).click();
+            });
         });
 
         step("Select industry", () -> {
-            // todo medicine
+            $("#customSelect__industry").click();
+            $$(".isOpen .customSelect__option").findBy(text(industry)).click();
         });
 
         step("Select location", () -> {
-            // todo Lublin
+            $("#customSelect__location").click();
+            $$(".isOpen .customSelect__option").findBy(text(location)).click();
         });
 
         step("Check default text", () -> {
-            // todo
+            $(".vacanciesWidget__error").shouldHave(text("По вашему запросу ничего не найдено"));
+            $(".vacanciesWidget__error").shouldHave(text("Убедитесь, что все слова написаны без ошибок, или попробуйте использовать другие ключевые слова"));
         });
     }
 }
