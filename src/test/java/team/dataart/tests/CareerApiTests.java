87afdfdee4bee22fad3facfd7c6fe8a226fd2325
Gorbatenko Vitaliy <gorvit.dp@gmail.com>
2021-05-11 01:01:57 +0300

    add parameters to allure api test

--- src/test/java/team/dataart/tests/CareerApiTests.java
+++ src/test/java/team/dataart/tests/CareerApiTests.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
+import static io.qameta.allure.Allure.parameter;
 import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.containsString;
@@ -30,17 +31,20 @@ public class CareerApiTests {
     @CsvFileSource(resources = "/csv/vacancySectionTags.csv", numLinesToSkip = 1)
     @DisplayName("Successful authorization with set cookie, received by API")
     void checkTechnologyTitles(String path, String value) {
-            given()
-                    .filter(filters().withCustomTemplates())
-                    .log().uri()
-                    .contentType("application/x-www-form-urlencoded; charset=UTF-8")
-                    .formParam("section", "vacancy")
-                    .when()
-                    .get("/ajax/getSectionTags")
-                    .then()
-                    .statusCode(200)
-                    .log().body()
-                    .assertThat()
-                    .body(path, containsString(value));
+        parameter("path", path);
+        parameter("value", value);
+
+        given()
+                .filter(filters().withCustomTemplates())
+                .log().uri()
+                .contentType("application/x-www-form-urlencoded; charset=UTF-8")
+                .formParam("section", "vacancy")
+                .when()
+                .get("/ajax/getSectionTags")
+                .then()
+                .statusCode(200)
+                .log().body()
+                .assertThat()
+                .body(path, containsString(value));
     }
 }
